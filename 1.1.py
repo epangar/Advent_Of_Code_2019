7ac642e82b20c5bf689895967435698990572b82
epangar <e.pan.gar@gmail.com>
2021-11-10 15:51:26 +0100

    Exercise 1.1

--- 1.1.py
+++ 1.1.py
@@ -1,8 +1,8 @@
 import math, functools
 
-def calculate_total_fuel_requirement(arr_input):
-    answer = functools.reduce(lambda a,b: a+b, [calculate_fuel(fuel) for fuel in arr_input])
-    return answer
+def calculate_total_requirement(arr_input):
+    #We use reduce() to add all the calculated fuel values in our input
+    return functools.reduce(lambda a,b: a+b, [calculate_fuel(fuel) for fuel in arr_input])
 
 def calculate_fuel(input):
     return (math.floor(input/3)-2)
